fbab1b6c15341e259141afab1257bb267ea9cec9
Voradech <142993443+Voradech@users.noreply.github.com>
2025-12-14 01:49:32 +0700

    s

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='flex justify-between'>
+        <div>s</div>
+        <div>sasdasd</div>
+         </div>
+    </div>
+  )
+}
+
+export default Navbar
