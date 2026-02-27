ad08f312e755f7c0aee1db1b03c2f6f26bb44350
Voradech <chunhapluckvoradech@gmail.com>
2026-02-28 01:45:01 +0700

    .

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const accessToken = req.cookies.get("accessToken")?.value;
   const role = req.cookies.get("role")?.value;
   const path = req.nextUrl.pathname;
